269a619bde29c59d4131118276e9f041deb5605c
stereynoatu <L00186143@atu.ie>
2023-11-05 14:26:56 +0000

    remove coverage and add unit test

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
-use_plugin("python.coverage")
+#use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
@@ -15,3 +15,4 @@ default_task = "publish"
 @init
 def set_properties(project):
     pass
+
